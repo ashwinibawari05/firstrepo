382b4416130cfabc043edec93095d2a642571992
Bawz <Bawz@192.168.0.104>
2023-04-23 15:08:13 +0530

    HI

---
+++ src/firstrepo1/SingletonDesignPattern.java
@@ -0,0 +1,15 @@
+package firstrepo1;
+
+class A {
+
+public static void SingletonDesignPattern (String[] args) {
+		static A a1= null;
+		private A(){
+
+		}
+		public static A getobject(){
+
+		}
+		
+}
+}
